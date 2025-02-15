44cfb40045cbf75854508c731e3cf1f5c14aef39
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-02-15 14:34:21 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 //% color="#5c70ff"
 namespace strings{
 let index = 0
-//% block="draw string x $x y $y width $width height $height color 0 $color_2 color 1 $color_1 string $string"
+//% block="draw string x $x y $y width $width height $height color 0 $color_2 color 1 $color_1 output $output string $string"
+//% output.shadow=screen_image_picker
 //% inlineInputMode=inline
-export function drawString (color_1: number, color_2: number, width: number, height: number, string: string, x: number, y: number) {
+export function drawString (color_1: number, color_2: number, width: number, height: number, string: string, x: number, y: number, output: Image) {
     index = 0
     for (let Y = 0; Y <= height - 1; Y++) {
         for (let X = 0; X <= width - 1; X++) {
