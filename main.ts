b19d18078562e81437ec5323d2680a90cdeaed96
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-02-15 13:25:55 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+//% color="#5c70ff"
+namespace strings{
+let index = 0
+//% block="draw string x $x y $y width $width height $height color 0 $color_2 color 1 $color_1 string $string"
+//% inlineInputMode=inline
+export function drawString (color_1: number, color_2: number, width: number, height: number, string: string, x: number, y: number) {
+    index = 0
+    for (let Y = 0; Y <= height - 1; Y++) {
+        for (let X = 0; X <= width - 1; X++) {
+            if (string.charAt(index) == "1") {
+                scene.backgroundImage().setPixel(X + x, Y + y, color_1)
+            } else {
+                scene.backgroundImage().setPixel(X + x, Y + y, color_2)
+            }
+            index += 1
+        }
+    }
+}
+}
